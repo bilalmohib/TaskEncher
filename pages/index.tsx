14fcf7d09329d10833eaf266d32c9a7503f63071
Forget <forget@forget.forget>
2022-11-15 12:13:55 +0500

    Working on sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,16 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
-       
+        {/* <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a> */}
       </footer>
     </div>
   )

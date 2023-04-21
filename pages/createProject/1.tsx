6d7d9f8fe16e0981279c7718af13387d36ce6055
AmmarMohib <ammarmohib7000@gmail.com>
2023-04-21 07:16:57 +0500

    New component

--- pages/createProject/1.tsx
+++ pages/createProject/1.tsx
@@ -2,8 +2,10 @@
 const SimpleComponent: React.FC = () => {
     return (
       <div>
-       <p>Hello, world!</p>
-        <h1>jhioyoiuyjhoihoi</h1>
+        <div style={{textAlign:'center'}}>
+       <h3>Create a new project</h3>
+       </div>
+        {/* <h1>jhioyoiuyjhoihoi</h1> */}
       </div>
     );
   };
---
+++ pages/createProject/2.tsx
@@ -0,0 +1,13 @@
+
+const SimpleComponent: React.FC = () => {
+    return (
+      <div>
+        <div style={{textAlign:'center'}}>
+       <h3>Create a new project</h3>
+       </div>
+        {/* <h1>jhioyoiuyjhoihoi</h1> */}
+      </div>
+    );
+  };
+  
+  export default SimpleComponent;

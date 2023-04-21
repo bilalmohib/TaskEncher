4f6fba9cb7752f4f236280592e2efcc2ddafd260
AmmarMohib <ammarmohib7000@gmail.com>
2023-04-21 07:59:50 +0500

    new line

--- pages/createProject/2.tsx
+++ pages/createProject/2.tsx
@@ -1,7 +1,11 @@
 
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 const SimpleComponent: React.FC = () => {
     return (
       <div>
+         <ArrowBackIcon />
+      <span>Back</span>
         <div style={{textAlign:'center'}}>
        <h3>Create a new project</h3>
        </div>

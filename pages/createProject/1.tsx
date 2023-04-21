dce09a5bfdd01ceb89640cd97bc838cdc8e3810e
AmmarMohib <ammarmohib7000@gmail.com>
2023-04-21 07:10:29 +0500

    New component

---
+++ pages/createProject/1.tsx
@@ -0,0 +1,11 @@
+
+const SimpleComponent: React.FC = () => {
+    return (
+      <div>
+       <p>Hello, world!</p>
+        <h1>jhioyoiuyjhoihoi</h1>
+      </div>
+    );
+  };
+  
+  export default SimpleComponent;
